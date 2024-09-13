2bcdcc02b8f64c6b3c08ad09472600f0459de0a5
malakhm <malakhmm22@gmail.com>
2024-09-13 14:35:25 +0300

    finished home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./Components/Header";
-import Home from "./Home";
+import Home from "./Pages/Home";
 
 function App() {
   return (
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles/home.css";
+import title from "../Assets/title.svg";
+import profile from "../Assets/profile.svg";
+import design from "../Assets/design.svg";
+
+const Home = () => {
+  return (
+    <div className="Home-main-component-class" id="Home">
+      <div className="Title-position-main ">
+        <div className="Title-position-main-container">
+        <span className="name-class">Hi I'm Malak Hamwi</span>
+        <div className="Title-main-container d-flex align-items-stretch">
+          <h1>I am a </h1>
+          <img className="title-img-svg" src={title} alt="title"/>
+        </div>
+        </div>
+        <img className="design-svg" src={design} alt="design"/>
+      </div>
+
+      <div className="Picture-and-element-main-container">
+        <img className="profile-main-component" src={profile} alt="profile"/>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
